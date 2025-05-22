a7274c0cd907d475dc3fce49cdb59a4cc59b2542
lowe25 <louiealdrin25@gmail.com>
2025-05-22 19:05:14 +0800

    test change src

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
-import './styles/style.css'
+import "./styles/style.css";
 
 function App() {
-
   return (
     <>
-    <div className='max-width'>
-      <h1 className="text-orange py-px text-blue-600 block text-[3rem]">Hello world</h1>
-      <p className="text-red-700 text-[1.6rem]">HELLO WORLD</p>
-    </div>
+      <div className="max-width">
+        <h1 className="text-orange py-px text-blue-600 block text-[3rem]">
+          Hello world
+        </h1>
+        <p className="text-red-700 text-[1.6rem]">HELLO WORLD</p>
+        <div className="h-screen bg-[red]">
+          <img src="./img/landing-carousel-1.jpg" alt="tetet" />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -11,6 +10,6 @@ export default defineConfig({
   //Remove for uploading
   base: "/test-page/",
   build: {
-    outDir: "docs"
-  }
-})
+    outDir: "docs",
+  },
+});
